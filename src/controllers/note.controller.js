79532ff406cbd6d376ead68cd271e47e41dcd625
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-06-07 18:32:33 +0530

    added notes controller

---
+++ src/controllers/note.controller.js
@@ -0,0 +1,34 @@
+import { User } from "../models/user.models.js";
+import { Project } from "../models/project.model.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constants";
+
+
+/**
+ NOTES controller
+ basic CRUD
+ getNotes
+ role based access to encrypted notes
+ */
+
+const createNote = asyncHandler(async(req, res) => {
+
+})
+
+const getNotes = asyncHandler(async(req, res) => {
+  
+})
+
+const updateNote = asyncHandler(async(req, res) => {
+  
+})
+
+const deleteNote = asyncHandler(async(req, res) => {
+  
+})
+
+const encryptNote = asyncHandler(async(req, res) => {
+  
+})
